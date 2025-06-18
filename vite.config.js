a0e728b8a275da4659a0f06fdb8ab9884d390919
Isra Fatima <fatimaisra464@gmail.com>
2025-06-18 12:02:25 +0500

    Fixed vite config and package.json for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/', // ⭐️ VERY IMPORTANT
+  base: '/MY_PORTFOLIO/',  // 👈 Replace with your repo name
   plugins: [react()],
-});
+})
